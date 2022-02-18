62d48ce0d8f439faa83d05a4f5cd568d4bfcb325
Victor Gao <vgao@fb.com>
2022-02-17 16:27:40 -0800

    [evm][exe-utils] implement mint, transfer_eth, execute_custom_code
    This implements some of the essential functionalities `evm-exec-utils`
    currently misses:
      - minting
      - transfer eth
      - ability to execute custom code without creating a contract
    
    Closes: #65

--- language/evm/exec-utils/src/tests.rs
+++ language/evm/exec-utils/src/tests.rs
@@ -3,7 +3,7 @@
 
 use crate::{compile::solc, exec::Executor};
 use anyhow::Result;
-use evm::{backend::MemoryVicinity, ExitReason};
+use evm::{backend::MemoryVicinity, ExitReason, Opcode};
 use primitive_types::{H160, U256};
 use std::path::{Path, PathBuf};
 
@@ -38,7 +38,8 @@ fn test_hello_world() -> Result<()> {
     let vicinity = test_vincinity();
     let mut exec = Executor::new(&vicinity);
 
-    assert!(exec.create_contract(H160::zero(), contract_code).is_ok());
+    let res = exec.create_contract(H160::zero(), contract_code);
+    assert!(res.is_ok());
 
     Ok(())
 }
@@ -57,11 +58,17 @@ fn test_two_functions() -> Result<()> {
         .create_contract(H160::zero(), contract_code)
         .expect("failed to create contract");
 
-    let (exit_reason, _buffer) =
-        exec.call_function(H160::zero(), contract_address, "do_nothing()", &[]);
+    let (exit_reason, _buffer) = exec.call_function(
+        H160::zero(),
+        contract_address,
+        0.into(),
+        "do_nothing()",
+        &[],
+    );
     assert!(matches!(exit_reason, ExitReason::Succeed(_)));
 
-    let (exit_reason, _buffer) = exec.call_function(H160::zero(), contract_address, "panic()", &[]);
+    let (exit_reason, _buffer) =
+        exec.call_function(H160::zero(), contract_address, 0.into(), "panic()", &[]);
     assert!(matches!(exit_reason, ExitReason::Revert(_)));
 
     Ok(())
@@ -88,6 +95,7 @@ fn test_a_plus_b() -> Result<()> {
     let (exit_reason, buffer) = exec.call_function(
         H160::zero(),
         contract_address,
+        0.into(),
         "plus(uint256,uint256)",
         &args,
     );
@@ -117,17 +125,82 @@ fn test_stateful() -> Result<()> {
 
     for _ in 0..7 {
         let (exit_reason, _buffer) =
-            exec.call_function(H160::zero(), contract_address, "inc()", &[]);
+            exec.call_function(H160::zero(), contract_address, 0.into(), "inc()", &[]);
         assert!(matches!(exit_reason, ExitReason::Succeed(_)));
     }
 
-    let (exit_reason, buffer) = exec.call_function(H160::zero(), contract_address, "get()", &[]);
+    let (exit_reason, buffer) =
+        exec.call_function(H160::zero(), contract_address, 0.into(), "get()", &[]);
     assert!(matches!(exit_reason, ExitReason::Succeed(_)));
 
     assert!(buffer.len() >= 32);
     let mut expected = [0u8; 32];
     expected[31] = 0x7;
     assert_eq!(&buffer[..32], &expected);
+
+    Ok(())
+}
+
+#[test]
+fn test_custom_code_add_two_numbers() -> Result<()> {
+    let vicinity = test_vincinity();
+    let mut exec = Executor::new(&vicinity);
+
+    let mut code = vec![];
+
+    // push 1
+    // push 2
+    // add
+    // push 0
+    // mstore
+    // push 32
+    // push 0
+    // return
+
+    code.extend([Opcode::PUSH1.0, 0x1, Opcode::PUSH1.0, 0x2, Opcode::ADD.0]);
+
+    code.push(Opcode::PUSH32.0);
+    code.extend([0; 32]);
+    code.push(Opcode::MSTORE.0);
+
+    let mut length = [0; 32];
+    length[31] = 32;
+    code.push(Opcode::PUSH32.0);
+    code.extend(length);
+    code.push(Opcode::PUSH32.0);
+    code.extend([0; 32]);
+    code.push(Opcode::RETURN.0);
+
+    let ret = exec
+        .execute_custom_code(H160::zero(), H160::zero(), code, vec![])
+        .expect("failed to execute code");
+
+    let mut expected = [0; 32];
+    expected[31] = 0x3;
+    assert_eq!(&ret, &expected);
+
+    Ok(())
+}
+
+#[test]
+fn test_transfer_eth() -> Result<()> {
+    let vicinity = test_vincinity();
+    let mut exec = Executor::new(&vicinity);
+
+    let alice_addr = H160::from([0x1; 20]);
+    let bob_addr = H160::from([0x2; 20]);
+
+    exec.mint(alice_addr, U256::from(5000))?;
+    exec.mint(bob_addr, U256::from(5000))?;
+
+    assert_eq!(exec.account_balance(alice_addr).unwrap(), U256::from(5000));
+    assert_eq!(exec.account_balance(bob_addr).unwrap(), U256::from(5000));
+
+    exec.transfer_eth(alice_addr, bob_addr, U256::from(1000))
+        .expect("failed to transfer eth");
+
+    assert_eq!(exec.account_balance(alice_addr).unwrap(), U256::from(4000));
+    assert_eq!(exec.account_balance(bob_addr).unwrap(), U256::from(6000));
 
     Ok(())
 }

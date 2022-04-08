064f758bf1f92f2febb90e303798f694d18765e4
Victor Gao <vgao@fb.com>
2022-04-08 08:29:53 -0700

    [evm] initial implementation of the Move plugin for hardhat

--- language/evm/hardhat-examples/hardhat.config.js
+++ language/evm/hardhat-examples/hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
 require("hardhat-gas-reporter");
+require("hardhat-move");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html

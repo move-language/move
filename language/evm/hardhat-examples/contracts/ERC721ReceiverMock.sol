87e5ad795d83c1595538aa8146574db7c624747a
Junkil Park <jkpark@fb.com>
2022-04-18 15:39:36 -0700

    [evm] Added ERC721 and ERC1155 Move contracts to the hardhat project (#52)
    - Added the Move contracts ERC721Mock and ERC1155Mock
    - Added the Solidity contracts ERC721Mock_Sol and ERC1155Mock_Sol
    - Checked that they all passed the Openzepplin testsuite
    
    - Ignored .js files in the broken link test

--- language/evm/hardhat-examples/contracts/ERC721ReceiverMock.sol
+++ language/evm/hardhat-examples/contracts/ERC721ReceiverMock.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "hardhat/console.sol";
+
 
 contract ERC721ReceiverMock is IERC721Receiver {
     enum Error {
@@ -29,14 +31,18 @@ contract ERC721ReceiverMock is IERC721Receiver {
         bytes memory data
     ) public override returns (bytes4) {
         if (_error == Error.RevertWithMessage) {
+            // console.log('RevertWithMessage');
             revert("ERC721ReceiverMock: reverting");
         } else if (_error == Error.RevertWithoutMessage) {
+            // console.log('RevertWithoutMessage');
             revert();
         } else if (_error == Error.Panic) {
+            // console.log('Panic');
             uint256 a = uint256(0) / uint256(0);
             a;
         }
         emit Received(operator, from, tokenId, data, gasleft());
+        //console.log('Ok');
         return _retval;
     }
 }

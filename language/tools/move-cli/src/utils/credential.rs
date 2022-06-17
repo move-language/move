dc79a3d5391f4a2038cb122b04bdfc57c67ddb8f
ea-open-source <open-source@eastagile.com>
2022-06-17 17:07:55 +0700

    Refactor move_cli's utils & add cli tests
    # Conflicts:
    #	language/tools/move-cli/tests/cli_tests.rs

--- language/tools/move-cli/src/utils/credential.rs
+++ language/tools/move-cli/src/utils/credential.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use anyhow::{Result, Context, bail};
-use toml_edit::easy::Value;
+use toml_edit::easy::{Value};
 
 pub fn get_move_home_path(is_test_mode: bool) -> String {
     let mut home = std::env::var("MOVE_HOME").unwrap_or_else(|_| {
@@ -20,25 +20,113 @@ pub fn get_credential_path(is_test_mode: bool) -> String {
 }
 
 pub fn get_registry_api_token(is_test_mode: bool) -> Result<String> {
-    match get_api_token(is_test_mode) {
-        Ok(content) => Ok(content),
-        Err(_) => bail!("There seems to be an error with your Movey credential. \
+    if let Ok(content) = get_api_token(is_test_mode) {
+        Ok(content)
+    } else {
+        bail!("There seems to be an error with your Movey credential. \
             Please run `move login` and follow the instructions.")
     }
 }
 
 fn get_api_token(is_test_mode: bool) -> Result<String> {
-    let move_home = get_move_home_path(is_test_mode);
-    let credential_path = move_home.clone() + "/credential.toml";
+    let credential_path = get_credential_path(is_test_mode);
     
     let contents = fs::read_to_string(&credential_path)?;
     let mut toml: Value = contents.parse()?;
     let registry = toml.as_table_mut()
-        .context("None")?
+        .context("Error parsing credential.toml")?
         .get_mut("registry")
-        .context("None")?;
-    let token = registry.as_table_mut().context("None")?
+        .context("Error parsing credential.toml")?;
+    let token = registry.as_table_mut()
+        .context("Error parsing token")?
         .get_mut("token")
-        .context("None")?;
+        .context("Error parsing token")?;
     Ok(token.to_string().replace("\"", ""))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use home::home_dir;
+    use std::env;
+    use std::fs::File;
+
+    fn setup_move_home() -> (String, String) {
+        let mut move_home = env::var("MOVE_HOME").unwrap_or_else(|_| {
+            env::var("HOME").unwrap_or_else(|_| {
+                let home_dir = home_dir().unwrap().to_string_lossy().to_string();
+                env::set_var("HOME", &home_dir);
+                home_dir
+            })
+        });
+        move_home.push_str("/.move/test");
+        let credential_path = move_home.clone() + "/credential.toml";
+
+        return (move_home, credential_path);
+    }
+
+    fn clean_up() {
+        let (move_home, _) = setup_move_home();
+        let _ = fs::remove_dir_all(move_home);
+    }
+
+    #[test]
+    fn get_api_token_works() {
+        let (move_home, credential_path) = setup_move_home();
+        
+        let _ = fs::create_dir_all(&move_home);
+        File::create(&credential_path).unwrap();
+
+        let content = "[registry]\ntoken = \"a sample token\"";
+        fs::write(&credential_path, content).unwrap();
+        
+        let token = get_api_token(true).unwrap();
+        assert!(token.contains("a sample token"));
+
+        clean_up()
+    }
+
+    #[test]
+    fn get_api_token_fails_if_there_is_no_move_home_directory() {
+        let (move_home, _) = setup_move_home();
+        let _ = fs::remove_dir_all(&move_home);
+        let token = get_api_token(true);
+        assert!(token.is_err());
+
+        clean_up()
+    }
+
+    #[test]
+    fn get_api_token_fails_if_there_is_no_credential_file() {
+        let (move_home, _) = setup_move_home();
+        let _ = fs::remove_dir_all(&move_home);
+        fs::create_dir_all(&move_home).unwrap();
+        let token = get_api_token(true);
+        assert!(token.is_err());
+
+        clean_up()
+    }
+
+    #[test]
+    fn get_api_token_fails_if_credential_file_is_in_wrong_format() {
+        let (move_home, credential_path) = setup_move_home();
+
+        let _ = fs::remove_dir_all(&move_home);
+        fs::create_dir_all(&move_home).unwrap();
+        File::create(&credential_path).unwrap();
+
+        let content = "[registry]\ntoken = a sample token";
+        fs::write(&credential_path, content).unwrap();
+
+        let token = get_api_token(true);
+        assert!(token.is_err());
+
+        let content = "[registry]\ntokens = \"a sample token\"";
+        fs::write(&credential_path, content).unwrap();
+
+        let token = get_api_token(true);
+        assert!(token.is_err());
+
+        clean_up()
+    }
 }
--- language/tools/move-cli/tests/cli_tests.rs
+++ language/tools/move-cli/tests/cli_tests.rs
@@ -39,6 +39,12 @@ fn run_metatest() {
     assert!(test::run_all(&path_metatest, &path_cli_binary, true, false).is_ok());
 }
 
+const PACKAGE_PATH: &str = "./tests/upload_tests/valid_package";
+#[cfg(debug_assertions)]
+const CLI_EXE: &str = "../../../../../../target/debug/move";
+#[cfg(not(debug_assertions))]
+const CLI_EXE: &str = "../../../../../../target/release/move";
+
 #[test]
 fn cross_process_locking_git_deps() {
     let cli_exe = env!("CARGO_BIN_EXE_move");
@@ -58,14 +64,18 @@ fn cross_process_locking_git_deps() {
     handle.join().unwrap();
 }
 
-const PACKAGE_PATH: &str = "./tests/upload_tests/valid_package";
-#[cfg(debug_assertions)]
-const CLI_EXE: &str = "../../../../../../target/debug/move";
-#[cfg(not(debug_assertions))]
-const CLI_EXE: &str = "../../../../../../target/release/move";
-
 #[test]
 fn upload_package_to_movey_works() {
+    let (home, credential_file) = setup_move_home();
+    let _ = fs::remove_file(&credential_file);
+
+    fs::create_dir_all(&home).unwrap();
+    let mut file = File::create(&credential_file).unwrap();
+    let credential_content = String::from(
+            "[registry]\ntoken=\"eb8xZkyr78FNL528j7q39zcdS6mxjBXt\"\n"
+    );
+    file.write(&credential_content.as_bytes()).unwrap();
+
     init_git(PACKAGE_PATH, 0);
     let output = Command::new(CLI_EXE)
         .current_dir(PACKAGE_PATH)
@@ -79,6 +89,8 @@ fn upload_package_to_movey_works() {
     assert!(data.contains("\"github_repo_url\":\"https://github.com/diem/move\""));
     assert!(data.contains("\"description\":\"Description test\""));
     fs::remove_file(&res_path).unwrap();
+
+    clean_up(&home);
 }
 
 #[test]
@@ -109,10 +121,9 @@ fn upload_package_to_movey_with_no_head_commit_id_should_panic() {
 
 #[test]
 fn upload_package_to_movey_with_no_credential_should_panic() {
-    let home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
-    let credential_file = home.clone() + "/credential.toml";
+    let (home, credential_file) = setup_move_home();
     let _ = fs::remove_file(&credential_file);
-    
+
     init_git(PACKAGE_PATH, 0);
     let output = Command::new(CLI_EXE)
         .current_dir(PACKAGE_PATH)
@@ -123,14 +134,15 @@ fn upload_package_to_movey_with_no_credential_should_panic() {
     let error = String::from_utf8_lossy(output.stderr.as_slice()).to_string();
     assert!(error.contains("There seems to be an error with your Movey credential. \
         Please run `move login` and follow the instructions."));
+
+    clean_up(&home);
 }
 
 #[test]
 fn upload_package_to_movey_with_bad_credential_should_panic() {
-    let home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
-    let credential_file = home.clone() + "/credential.toml";
+    let (home, credential_file) = setup_move_home();
     let _ = fs::remove_file(&credential_file);
-    
+
     fs::create_dir_all(&home).unwrap();
     let mut file = File::create(&credential_file).unwrap();
     let bad_content = String::from("[registry]\ntoken=\n");
@@ -146,6 +158,8 @@ fn upload_package_to_movey_with_bad_credential_should_panic() {
     let error = String::from_utf8_lossy(output.stderr.as_slice()).to_string();
     assert!(error.contains("There seems to be an error with your Movey credential. \
         Please run `move login` and follow the instructions."));
+
+    clean_up(&home);
 }
 
 // flag == 0: all git command are run
@@ -180,4 +194,14 @@ fn init_git(package_path: &str, flag: i32) {
             .args(&["commit", "-m", "initial commit"])
             .output().unwrap();
     }
+}
+
+fn clean_up(move_home: &str) {
+    let _ = fs::remove_dir_all(move_home);
+}
+
+fn setup_move_home() -> (String, String) {
+    let home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
+    let credential_file = home.clone() + "/credential.toml";
+    (home, credential_file)
 }

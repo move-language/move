b006e8f6ca303971dffefd8aaaf3054f17dc97ff
Owen Wu <yubing744@163.com>
2022-06-26 15:01:07 +0000

    feat: ignore yarn.lock and recover configuration.ts

--- language/move-analyzer/editors/code/src/configuration.ts
+++ language/move-analyzer/editors/code/src/configuration.ts
@@ -4,7 +4,7 @@
 
 import * as os from 'os';
 import * as vscode from 'vscode';
-import * as path from 'path';
+import * as Path from 'path';
 
 /**
  * User-defined configuration values, such as those specified in VS Code settings.
@@ -26,19 +26,17 @@ export class Configuration {
 
     /** The path to the move-analyzer executable. */
     get serverPath(): string {
-        const defaultName = '~/.cargo/bin/move-analyzer';
-        let serverPath = this.configuration.get<string>('server.path', defaultName);
-        if (serverPath.length === 0) {
+        const defaultName = 'move-analyzer';
+        const path = this.configuration.get<string>('server.path', defaultName);
+        if (path.length === 0) {
             // The default value of the `server.path` setting is 'move-analyzer'.
             // A user may have over-written this default with an empty string value, ''.
             // An empty string cannot be an executable name, so instead use the default.
-            serverPath = defaultName;
+            return defaultName;
         }
-
-        if (serverPath.startsWith('~/')) {
-            return os.homedir() + serverPath.slice('~'.length);
+        if (path.startsWith('~/')) {
+            return os.homedir() + path.slice('~'.length);
         }
-
-        return path.resolve(serverPath);
+        return Path.resolve(path);
     }
 }

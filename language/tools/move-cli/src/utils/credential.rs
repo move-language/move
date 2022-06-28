2e6f62f3decc8713a68d18a6e63b0c489c72c0a5
ea-open-source <open-source@eastagile.com>
2022-06-28 17:38:13 +0700

    Refactor test code to run in parallel

--- language/tools/move-cli/src/utils/credential.rs
+++ language/tools/move-cli/src/utils/credential.rs
@@ -1,26 +1,40 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::{bail, Context, Result};
 use std::fs;
 use toml_edit::easy::Value;
 
-pub fn get_move_home_path(is_test_mode: bool) -> String {
-    let mut home = std::env::var("MOVE_HOME").unwrap_or_else(|_| {
+#[derive(Clone)]
+pub struct TestMode {
+    pub test_path: String,
+}
+
+pub fn get_move_home_path(test_mode: Option<TestMode>) -> String {
+    let mut move_home = std::env::var("MOVE_HOME").unwrap_or_else(|_| {
         format!(
             "{}/.move",
             std::env::var("HOME").expect("env var 'HOME' must be set")
         )
     });
-    if is_test_mode && !home.contains("/test") {
-        home.push_str("/test");
+    if let Some(test_mode) = test_mode {
+        if !move_home.contains("/test") {
+            move_home.push_str("/test");
+        }
+        if !test_mode.test_path.is_empty() {
+            move_home.push_str(&test_mode.test_path);
+        }
     }
-    home
+    move_home
 }
 
-pub fn get_credential_path(is_test_mode: bool) -> String {
-    get_move_home_path(is_test_mode) + "/credential.toml"
+pub fn get_credential_path(test_mode: Option<TestMode>) -> String {
+    get_move_home_path(test_mode) + "/credential.toml"
 }
 
-pub fn get_registry_api_token(is_test_mode: bool) -> Result<String> {
-    if let Ok(content) = get_api_token(is_test_mode) {
+pub fn get_registry_api_token(test_mode: Option<TestMode>) -> Result<String> {
+    if let Ok(content) = get_api_token(test_mode) {
         Ok(content)
     } else {
         bail!(
@@ -30,8 +44,8 @@ pub fn get_registry_api_token(is_test_mode: bool) -> Result<String> {
     }
 }
 
-fn get_api_token(is_test_mode: bool) -> Result<String> {
-    let credential_path = get_credential_path(is_test_mode);
+fn get_api_token(test_mode: Option<TestMode>) -> Result<String> {
+    let credential_path = get_credential_path(test_mode);
 
     let contents = fs::read_to_string(&credential_path)?;
     let mut toml: Value = contents.parse()?;
@@ -55,7 +69,7 @@ mod tests {
     use std::env;
     use std::fs::File;
 
-    fn setup_move_home() -> (String, String) {
+    fn setup_move_home(test_path: &str) -> (String, String) {
         let mut move_home = env::var("MOVE_HOME").unwrap_or_else(|_| {
             env::var("HOME").unwrap_or_else(|_| {
                 let home_dir = home_dir().unwrap().to_string_lossy().to_string();
@@ -64,19 +78,23 @@ mod tests {
             })
         });
         move_home.push_str("/.move/test");
+        if !test_path.is_empty() {
+            move_home.push_str(&test_path);
+        }
+
         let credential_path = move_home.clone() + "/credential.toml";
 
         return (move_home, credential_path);
     }
 
-    fn clean_up() {
-        let (move_home, _) = setup_move_home();
+    fn clean_up(move_home: &str) {
         let _ = fs::remove_dir_all(move_home);
     }
 
     #[test]
     fn get_api_token_works() {
-        let (move_home, credential_path) = setup_move_home();
+        let test_path = String::from("/get_api_token_works");
+        let (move_home, credential_path) = setup_move_home(&test_path);
 
         let _ = fs::create_dir_all(&move_home);
         File::create(&credential_path).unwrap();
@@ -84,36 +102,42 @@ mod tests {
         let content = "[registry]\ntoken = \"a sample token\"";
         fs::write(&credential_path, content).unwrap();
 
-        let token = get_api_token(true).unwrap();
+        let test_mode = TestMode { test_path };
+        let token = get_api_token(Some(test_mode)).unwrap();
         assert!(token.contains("a sample token"));
 
-        clean_up()
+        clean_up(&move_home)
     }
 
     #[test]
     fn get_api_token_fails_if_there_is_no_move_home_directory() {
-        let (move_home, _) = setup_move_home();
+        let test_path = String::from("/get_api_token_fails_if_there_is_no_move_home_directory");
+        let (move_home, _) = setup_move_home(&test_path);
         let _ = fs::remove_dir_all(&move_home);
-        let token = get_api_token(true);
+        let test_mode = TestMode { test_path };
+        let token = get_api_token(Some(test_mode));
         assert!(token.is_err());
 
-        clean_up()
+        clean_up(&move_home)
     }
 
     #[test]
     fn get_api_token_fails_if_there_is_no_credential_file() {
-        let (move_home, _) = setup_move_home();
+        let test_path = String::from("/get_api_token_fails_if_there_is_no_credential_file");
+        let (move_home, _) = setup_move_home(&test_path);
         let _ = fs::remove_dir_all(&move_home);
         fs::create_dir_all(&move_home).unwrap();
-        let token = get_api_token(true);
+        let test_mode = TestMode { test_path };
+        let token = get_api_token(Some(test_mode));
         assert!(token.is_err());
 
-        clean_up()
+        clean_up(&move_home)
     }
 
     #[test]
     fn get_api_token_fails_if_credential_file_is_in_wrong_format() {
-        let (move_home, credential_path) = setup_move_home();
+        let test_path = String::from("/get_api_token_fails_if_credential_file_is_in_wrong_format");
+        let (move_home, credential_path) = setup_move_home(&test_path);
 
         let _ = fs::remove_dir_all(&move_home);
         fs::create_dir_all(&move_home).unwrap();
@@ -122,15 +146,16 @@ mod tests {
         let content = "[registry]\ntoken = a sample token";
         fs::write(&credential_path, content).unwrap();
 
-        let token = get_api_token(true);
+        let test_mode = TestMode { test_path };
+        let token = get_api_token(Some(test_mode.clone()));
         assert!(token.is_err());
 
         let content = "[registry]\ntokens = \"a sample token\"";
         fs::write(&credential_path, content).unwrap();
 
-        let token = get_api_token(true);
+        let token = get_api_token(Some(test_mode));
         assert!(token.is_err());
 
-        clean_up()
+        clean_up(&move_home)
     }
 }

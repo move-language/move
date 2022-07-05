6bdc2168e8b077c54214ac788fedc46829c833b6
Todd Nowacki <tmn@mystenlabs.com>
2022-07-05 11:26:27 -0700

    fixup! fixup! fixup! fixup! Flatten package commands

--- language/documentation/examples/diem-framework/prove_all.sh
+++ language/documentation/examples/diem-framework/prove_all.sh
@@ -5,6 +5,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd "${SCRIPT_DIR}/move-packages/DPN" && cargo run -p df-cli -- package prove &&
-cd "${SCRIPT_DIR}/move-packages/core" && cargo run -p df-cli -- package prove &&
-cd "${SCRIPT_DIR}/move-packages/experimental" && cargo run -p df-cli -- package prove
+cd "${SCRIPT_DIR}/move-packages/DPN" && cargo run -p df-cli -- prove &&
+cd "${SCRIPT_DIR}/move-packages/core" && cargo run -p df-cli -- prove &&
+cd "${SCRIPT_DIR}/move-packages/experimental" && cargo run -p df-cli -- prove

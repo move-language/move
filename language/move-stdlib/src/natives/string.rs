a8383d88fa48f4e1e0e91264cffbbd27136e4732
Wolfgang Grieskamp <wgrieskamp@gmail.com>
2022-07-08 13:00:33 -0700

    [stdlib] UTF8 String type (#237)
    * [stdlib] UTF8 String type
    
    This introduces a new type `string::String` which represents a valid UTF8 string. The type is intended to replace `ascii::string`.
    
    The UTF8 enforcement is based on Rust's and correct only modulo Rust. When creating a string with `string::utf8(bytes)`
    the Rust `str` implementation will check the correctness of the encoding. On deconstructing or composing strings, the Rust function
    `str::is_valid_char_boundary` is used to allow composition only on utf8 boundaries. The String type does not allow direct mutation of
    bytes, therefore utf8 correctness is not violated in other ways.
    
    Currently, the only way to denote a string is as in `string::utf8(b"hello")`. If someone would want to create unicode characters, they
    would have to use the `\x1FH` escape in the Move string syntax. We may want to consider introducing string literal `u"foo"` which can directly be used to
    denote strings, and which also supports `\u{digits}` style of unicode escapes.
    
    * Addressing reviewer comments.
    
    * Adding a test that BCS serialization of a boxed value is equivalent to the box
    
    * Remove superfluous import

---
+++ language/move-stdlib/src/natives/string.rs
@@ -0,0 +1,98 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implementation of native functions for utf8 strings.
+
+use move_binary_format::errors::PartialVMResult;
+use move_vm_runtime::native_functions::NativeContext;
+use move_vm_types::{
+    gas_schedule::NativeCostIndex,
+    loaded_data::runtime_types::Type,
+    natives::function::{native_gas, NativeResult},
+    pop_arg,
+    values::{Value, VectorRef},
+};
+
+use std::collections::VecDeque;
+
+// The implementation approach delegates all utf8 handling to Rust.
+// This is possible without copying of bytes because (a) we can
+// get a `std::cell::Ref<Vec<u8>>` from a `vector<u8>` and in turn a `&[u8]`
+// from that (b) assuming that `vector<u8>` embedded in a string
+// is already valid utf8, we can use `str::from_utf8_unchecked` to
+// create a `&str` view on the bytes without a copy. Once we have this
+// view, we can call ut8 functions like length, substring, etc.
+
+pub fn native_check_utf8(
+    context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(args.len() == 1);
+    let s_arg = pop_arg!(args, VectorRef);
+    let s_ref = s_arg.as_bytes_ref();
+    let ok = std::str::from_utf8(s_ref.as_slice()).is_ok();
+    // TODO: extensible native cost tables
+    let cost = native_gas(context.cost_table(), NativeCostIndex::PUSH_BACK, 1);
+    NativeResult::map_partial_vm_result_one(cost, Ok(Value::bool(ok)))
+}
+
+pub fn native_is_char_boundary(
+    context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(args.len() == 2);
+    let i = pop_arg!(args, u64);
+    let s_arg = pop_arg!(args, VectorRef);
+    let s_ref = s_arg.as_bytes_ref();
+    let ok = unsafe {
+        // This is safe because we guarantee the bytes to be utf8.
+        std::str::from_utf8_unchecked(s_ref.as_slice()).is_char_boundary(i as usize)
+    };
+    // TODO: extensible native cost tables
+    let cost = native_gas(context.cost_table(), NativeCostIndex::PUSH_BACK, 1);
+    NativeResult::map_partial_vm_result_one(cost, Ok(Value::bool(ok)))
+}
+
+pub fn native_sub_string(
+    context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(args.len() == 3);
+    let j = pop_arg!(args, u64) as usize;
+    let i = pop_arg!(args, u64) as usize;
+    let s_arg = pop_arg!(args, VectorRef);
+    let s_ref = s_arg.as_bytes_ref();
+    let s_str = unsafe {
+        // This is safe because we guarantee the bytes to be utf8.
+        std::str::from_utf8_unchecked(s_ref.as_slice())
+    };
+    let v = Value::vector_u8((&s_str[i..j]).as_bytes().iter().cloned());
+    // TODO: extensible native cost tables
+    let cost = native_gas(context.cost_table(), NativeCostIndex::PUSH_BACK, 1);
+    NativeResult::map_partial_vm_result_one(cost, Ok(v))
+}
+
+pub fn native_index_of(
+    context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(args.len() == 2);
+    let r_arg = pop_arg!(args, VectorRef);
+    let r_ref = r_arg.as_bytes_ref();
+    let r_str = unsafe { std::str::from_utf8_unchecked(r_ref.as_slice()) };
+    let s_arg = pop_arg!(args, VectorRef);
+    let s_ref = s_arg.as_bytes_ref();
+    let s_str = unsafe { std::str::from_utf8_unchecked(s_ref.as_slice()) };
+    let pos = match s_str.find(r_str) {
+        Some(size) => size,
+        None => s_str.len(),
+    };
+    // TODO: extensible native cost tables
+    let cost = native_gas(context.cost_table(), NativeCostIndex::LENGTH, 1);
+    NativeResult::map_partial_vm_result_one(cost, Ok(Value::u64(pos as u64)))
+}

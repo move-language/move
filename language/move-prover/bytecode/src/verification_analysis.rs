84eba3e639908136f3aca95d03ed5abb7b1f235f
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-09-15 02:39:01 -0400

    [move-prover] allow recursive move function
    Recursive Move functions are not currently supported in Move prover.
    
    This is because two passes in the transformation pipeline does not
    support it:
    - borrow analysis does not support recursive functions that returns
      mutable references (`&mut`), and
    - verification analysis does not know whether to mark a recursive
      function (or a mutually recursive function) inline or verify.
    
    This commit does not add any functionality to support either analysis,
    but does allow the following situation to pass:
    
    - if a recursive Move function does not return `&mut`, we allow it to
      pass the borrow analysis as it does not affect the borrow graph.
    - Check that all recursive Move functions should be marked with
      `pragma opaque`. In this way, we essentially bypass the problem of
      whether to generate a Boogie `function` or `procedure` for a
      recursive Move function. We generate the VC for the recursive
      function by inlining its spec at the recursive call sites (like how we
      treat any other opaque function)

--- language/move-prover/bytecode/src/verification_analysis.rs
+++ language/move-prover/bytecode/src/verification_analysis.rs
@@ -7,12 +7,13 @@
 //! each function as well as collect information on how these invariants should be handled (i.e.,
 //! checked after bytecode, checked at function exit, or deferred to caller).
 
-use crate::{
-    function_target::{FunctionData, FunctionTarget},
-    function_target_pipeline::{FunctionTargetProcessor, FunctionTargetsHolder, FunctionVariant},
-    options::ProverOptions,
-    usage_analysis,
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fmt::{self, Formatter},
 };
+
+use codespan_reporting::diagnostic::Severity;
+use itertools::Itertools;
 
 use move_model::{
     ast::GlobalInvariant,
@@ -24,11 +25,11 @@ use move_model::{
     ty::{TypeUnificationAdapter, Variance},
 };
 
-use codespan_reporting::diagnostic::Severity;
-use itertools::Itertools;
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    fmt::{self, Formatter},
+use crate::{
+    function_target::{FunctionData, FunctionTarget},
+    function_target_pipeline::{FunctionTargetProcessor, FunctionTargetsHolder, FunctionVariant},
+    options::ProverOptions,
+    usage_analysis,
 };
 
 /// The annotation for information about verification.
@@ -386,13 +387,22 @@ impl VerificationAnalysisProcessor {
 
         // at this time, we only have the `baseline` variant in the targets
         let variant = FunctionVariant::Baseline;
-        let data = targets
-            .get_data_mut(&fun_env.get_qualified_id(), &variant)
-            .expect("function data defined");
-        let info = data.annotations.get_or_default_mut::<VerificationInfo>();
-        if !info.inlined {
-            info.inlined = true;
-            Self::mark_callees_inlined(fun_env, targets);
+        if let Some(data) = targets.get_data_mut(&fun_env.get_qualified_id(), &variant) {
+            let info = data.annotations.get_or_default_mut::<VerificationInfo>();
+            if !info.inlined {
+                info.inlined = true;
+                Self::mark_callees_inlined(fun_env, targets);
+            }
+        } else {
+            fun_env.module_env.env.error(
+                &fun_env.get_loc(),
+                &format!(
+                    "function `{}` is a recursive function \
+                       (or part of a mutually recursive function group) \
+                       and should be marked as opaque",
+                    fun_env.get_full_name_str()
+                ),
+            );
         }
     }
 

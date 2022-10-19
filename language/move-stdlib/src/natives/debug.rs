84506a4fd3cc5dc335070c71ab692e9690f2b555
Alin Tomescu <alinush@mit.edu>
2022-10-19 15:29:06 -0700

    added support to print UTF8-encoded std::string::String's via std::debug::print

--- language/move-stdlib/src/natives/debug.rs
+++ language/move-stdlib/src/natives/debug.rs
@@ -4,10 +4,13 @@
 
 use crate::natives::helpers::make_module_natives;
 use move_binary_format::errors::PartialVMResult;
-use move_core_types::gas_algebra::InternalGas;
+use move_core_types::{
+    account_address::AccountAddress, gas_algebra::InternalGas, language_storage::TypeTag,
+};
 use move_vm_runtime::native_functions::{NativeContext, NativeFunction};
+use move_vm_types::values::VectorRef;
 #[allow(unused_imports)]
-use move_vm_types::values::{values_impl::debug::print_reference, Reference};
+use move_vm_types::values::{values_impl::debug::print_reference, Reference, StructRef};
 #[allow(unused_imports)]
 use move_vm_types::{
     loaded_data::runtime_types::Type, natives::function::NativeResult, pop_arg, values::Value,
@@ -25,6 +28,23 @@ pub struct PrintGasParameters {
     pub base_cost: InternalGas,
 }
 
+#[cfg(feature = "testing")]
+fn is_string_type(context: &NativeContext, ty: &Type, move_std_addr: AccountAddress) -> bool {
+    let ty_tag = match context.type_to_type_tag(ty) {
+        Ok(ty_tag) => ty_tag,
+        Err(_) => return false,
+    };
+
+    match ty_tag {
+        TypeTag::Struct(struct_tag) => {
+            struct_tag.address == move_std_addr
+                && struct_tag.module.as_str().eq("string")
+                && struct_tag.name.as_str().eq("String")
+        }
+        _ => false,
+    }
+}
+
 #[allow(unused_mut)]
 #[inline]
 fn native_print(
@@ -32,6 +52,7 @@ fn native_print(
     _context: &mut NativeContext,
     mut ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
+    move_std_addr: AccountAddress,
 ) -> PartialVMResult<NativeResult> {
     debug_assert!(ty_args.len() == 1);
     debug_assert!(args.len() == 1);
@@ -39,21 +60,42 @@ fn native_print(
     // No-op if the feature flag is not present.
     #[cfg(feature = "testing")]
     {
-        let _ty = ty_args.pop().unwrap();
-        let r = pop_arg!(args, Reference);
+        let ty = ty_args.pop().unwrap();
 
-        let mut buf = String::new();
-        print_reference(&mut buf, &r)?;
-        println!("[debug] {}", buf);
+        match is_string_type(_context, &ty, move_std_addr) {
+            true => {
+                let r = pop_arg!(args, StructRef);
+
+                let field_ref = r
+                    .borrow_field(0)? // borrows the 'bytes' field of the std::string::String struct
+                    .value_as::<VectorRef>()?;
+
+                let bytes = field_ref.as_bytes_ref();
+
+                // This is safe because we guarantee the bytes to be utf8.
+                let str = unsafe { std::str::from_utf8_unchecked(bytes.as_slice()) };
+
+                println!("[debug] {}", str);
+            }
+            false => {
+                let r = pop_arg!(args, Reference);
+                let mut buf = String::new();
+                print_reference(&mut buf, &r)?;
+                println!("[debug] {}", buf);
+            }
+        }
     }
 
     Ok(NativeResult::ok(gas_params.base_cost, smallvec![]))
 }
 
-pub fn make_native_print(gas_params: PrintGasParameters) -> NativeFunction {
+pub fn make_native_print(
+    gas_params: PrintGasParameters,
+    move_std_addr: AccountAddress,
+) -> NativeFunction {
     Arc::new(
         move |context, ty_args, args| -> PartialVMResult<NativeResult> {
-            native_print(&gas_params, context, ty_args, args)
+            native_print(&gas_params, context, ty_args, args, move_std_addr)
         },
     )
 }
@@ -106,9 +148,12 @@ pub struct GasParameters {
     pub print_stack_trace: PrintStackTraceGasParameters,
 }
 
-pub fn make_all(gas_params: GasParameters) -> impl Iterator<Item = (String, NativeFunction)> {
+pub fn make_all(
+    gas_params: GasParameters,
+    move_std_addr: AccountAddress,
+) -> impl Iterator<Item = (String, NativeFunction)> {
     let natives = [
-        ("print", make_native_print(gas_params.print)),
+        ("print", make_native_print(gas_params.print, move_std_addr)),
         (
             "print_stack_trace",
             make_native_print_stack_trace(gas_params.print_stack_trace),

77c454c01c24504e73400fb580bf73f3706aeeb7
Victor Gao <vgao1996@gmail.com>
2022-11-17 20:33:49 +0000

    [vm] fix fuzzing for vm values

--- language/move-vm/types/src/values/values_impl.rs
+++ language/move-vm/types/src/values/values_impl.rs
@@ -3461,7 +3461,7 @@ pub mod prop {
             L::U32 => any::<u32>().prop_map(Value::u32).boxed(),
             L::U64 => any::<u64>().prop_map(Value::u64).boxed(),
             L::U128 => any::<u128>().prop_map(Value::u128).boxed(),
-            L::U256 => any::<u256>().prop_map(Value::u256).boxed(),
+            L::U256 => any::<u256::U256>().prop_map(Value::u256).boxed(),
             L::Bool => any::<bool>().prop_map(Value::bool).boxed(),
             L::Address => any::<AccountAddress>().prop_map(Value::address).boxed(),
             L::Signer => any::<AccountAddress>().prop_map(Value::signer).boxed(),

317efe8e93e1cde7b36566060ceb95adb526fe86
Ashok Menon <ashok@mystenlabs.com>
2022-11-25 15:16:14 +0000

    fixup: More detailed documentation for verify_reducibility

--- language/move-bytecode-verifier/src/control_flow.rs
+++ language/move-bytecode-verifier/src/control_flow.rs
@@ -35,6 +35,34 @@ pub fn verify_fallthrough(
     }
 }
 
+/// Test that `function_view`'s control-flow graph is reducible using Tarjan's algorithm [1].
+/// Optionally test loop depth bounded by `verifier_config.max_loop_depth`.
+///
+/// A CFG, `G`, with starting block `s` is reducible if and only if [2] any of the following
+/// equivalent properties hold:
+///
+///  1. G has a unique set of back-edges `u -> v`, where the where `v` dominates `u` that
+///     corresponds to the set of back-edges for any depth-first spanning tree of G.
+///
+///  2. Every loop in G contains a unique node `h` (the "head") which dominates all other nodes in
+///     the loop.
+///
+///  3. G has a unique maximal (in terms of number of edges) acyclic sub-graph.
+///
+///  4. G can be reduced to a CFG containing just `s` through a sequence of the following two
+///     operations:
+///      a. Delete a cyclic edge `v -> v`
+///      b. For an edge `e: u -> v` where `e` is the only incident edge to `v`, collapse `v` into `u`
+///         by deleting `e` and `v` and replacing all `v -> w` edges with `u -> w` edges.
+///
+/// Reducibility means that a control-flow graph can be decomposed into a series of nested loops
+/// (strongly connected subgraphs), which leads to more predictable abstract interpretation
+/// performance.
+///
+/// ## References
+///
+///  1. Tarjan, R.  1974.  Testing Flow Graph Reducibility.
+///  2. Hecht, M. S., Ullman J. D.  1974.  Characterizations of Reducible Flow Graphs.
 pub fn verify_reducibility<'a>(
     verifier_config: &VerifierConfig,
     function_view: &'a FunctionView<'a>,
@@ -47,11 +75,18 @@ pub fn verify_reducibility<'a>(
     let summary = LoopSummary::new(function_view.cfg());
     let mut partition = LoopPartition::new(&summary);
 
+    // Iterate through nodes in reverse pre-order so more deeply nested loops (which would appear
+    // later in the pre-order) are processed first.
     for head in summary.preorder().rev() {
+        // If a node has no back edges, it is not a loop head, so doesn't need to be processed.
         let back = summary.back_edges(head);
         if back.is_empty() {
             continue;
         }
+
+        // Collect the rest of the nodes in `head`'s loop, in `body`.  Start with the nodes that
+        // jump back to the head, and grow `body` by repeatedly following predecessor edges until
+        // `head` is found again.
 
         let mut body = BTreeSet::new();
         for node in back {
@@ -66,6 +101,11 @@ pub fn verify_reducibility<'a>(
         while let Some(node) = frontier.pop() {
             for pred in summary.pred_edges(node) {
                 let pred = partition.containing_loop(*pred);
+
+                // `pred` can eventually jump back to `head`, so is part of its body.  If it is not
+                // a descendant of `head`, it implies that `head` does not dominate a node in its
+                // loop, therefore the CFG is not reducible, according to Property 1 (see doc
+                // comment).
                 if !summary.is_descendant(/* ancestor */ head, /* descendant */ pred) {
                     return err(StatusCode::INVALID_LOOP_SPLIT, summary.block(pred));
                 }
@@ -77,6 +117,8 @@ pub fn verify_reducibility<'a>(
             }
         }
 
+        // Collapse all the nodes in `body` into `head`, so it appears as one node when processing
+        // outer loops (this performs a sequence of Operation 4(b), followed by a 4(a)).
         let depth = partition.collapse_loop(head, &body);
         if let Some(max_depth) = verifier_config.max_loop_depth {
             if depth as usize > max_depth {

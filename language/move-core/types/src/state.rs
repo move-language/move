bb946cc5cf86a1146f126da11d326427448adfce
Robert <me@robertchen.cc>
2022-12-14 11:49:23 +0100

    [misc] add license to move-core state.rs

--- language/move-core/types/src/state.rs
+++ language/move-core/types/src/state.rs
@@ -1,3 +1,7 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 use std::cell::RefCell;
 
 #[derive(Clone, Copy, Debug)]

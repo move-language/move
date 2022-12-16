70c811f3187cd30c8e2195dd797fe052cff99ea7
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-12-16 10:52:42 -0500

    [move-prover] explicit well-formed assumption after havoc
    Currently well-formed check is instrumented at Boogie translation step.
    This commit explicitly instrument the well-formed check right after the
    havoc bytecode.
    
    This should be semantically equivalent in terms of loop handling, but
    greatly simplifies data invariant instrumentation as the data invariant
    instrumentation pass looks for `WellFormed` opcode as hook-up points to
    instrument the data invariants.
    
    This commit closes #716.

--- language/move-prover/bytecode/src/loop_analysis.rs
+++ language/move-prover/bytecode/src/loop_analysis.rs
@@ -9,6 +9,7 @@ use move_model::{
     ast::{self, TempIndex},
     exp_generator::ExpGenerator,
     model::FunctionEnv,
+    ty::{PrimitiveType, Type},
 };
 
 use crate::{
@@ -155,6 +156,13 @@ impl LoopAnalysisProcessor {
                                     None,
                                 )
                             });
+                            // add a well-formed assumption explicitly and immediately
+                            let exp = builder.mk_call(
+                                &Type::Primitive(PrimitiveType::Bool),
+                                ast::Operation::WellFormed,
+                                vec![builder.mk_temporary(*idx)],
+                            );
+                            builder.emit_with(move |id| Bytecode::Prop(id, PropKind::Assume, exp));
                         }
                         for (idx, havoc_all) in &loop_info.mut_targets {
                             let havoc_kind = if *havoc_all {
@@ -171,6 +179,13 @@ impl LoopAnalysisProcessor {
                                     None,
                                 )
                             });
+                            // add a well-formed assumption explicitly and immediately
+                            let exp = builder.mk_call(
+                                &Type::Primitive(PrimitiveType::Bool),
+                                ast::Operation::WellFormed,
+                                vec![builder.mk_temporary(*idx)],
+                            );
+                            builder.emit_with(move |id| Bytecode::Prop(id, PropKind::Assume, exp));
                         }
 
                         // trace implicitly reassigned variables after havocking

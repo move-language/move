63d568c09ccc4733b27bd522b195b34d1ed811d3
zhoujun.ma <zhoujun@aptoslabs.com>
2022-12-28 23:58:49 -0800

    fmt fix

--- language/move-vm/types/src/values/value_tests.rs
+++ language/move-vm/types/src/values/value_tests.rs
@@ -220,5 +220,8 @@ fn legacy_val_abstract_memory_size_consistency() -> PartialVMResult<()> {
 
 #[test]
 fn test_vm_value_vector_u64_casting() {
-    assert_eq!(vec![1, 2, 3], Value::vector_u64([1, 2, 3]).value_as::<Vec<u64>>().unwrap());
+    assert_eq!(
+        vec![1, 2, 3],
+        Value::vector_u64([1, 2, 3]).value_as::<Vec<u64>>().unwrap()
+    );
 }

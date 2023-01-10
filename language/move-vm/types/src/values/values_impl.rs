469a6be033b394c8281305fc74879b14360c652c
Teng Zhang <rahxephon89@163.com>
2023-01-10 14:06:10 -0800

    Cherry-pick onto main (#813)
    * [vm] fix memory leaks caused by errors
    
    * [vm] fix infinite recursion caused by printing out reference values
    
    * fix format
    
    Co-authored-by: Victor Gao <vgao1996@gmail.com>

--- language/move-vm/types/src/values/values_impl.rs
+++ language/move-vm/types/src/values/values_impl.rs
@@ -2626,16 +2626,41 @@ where
     write!(f, "]")
 }
 
+impl Container {
+    fn raw_address(&self) -> usize {
+        use Container::*;
+
+        match self {
+            Locals(r) => r.as_ptr() as usize,
+            Vec(r) => r.as_ptr() as usize,
+            Struct(r) => r.as_ptr() as usize,
+            VecU8(r) => r.as_ptr() as usize,
+            VecU16(r) => r.as_ptr() as usize,
+            VecU32(r) => r.as_ptr() as usize,
+            VecU64(r) => r.as_ptr() as usize,
+            VecU128(r) => r.as_ptr() as usize,
+            VecU256(r) => r.as_ptr() as usize,
+            VecBool(r) => r.as_ptr() as usize,
+            VecAddress(r) => r.as_ptr() as usize,
+        }
+    }
+}
+
+impl Locals {
+    pub fn raw_address(&self) -> usize {
+        self.0.as_ptr() as usize
+    }
+}
+
 impl Display for ContainerRef {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::Local(c) => write!(f, "({}, {})", c.rc_count(), c),
+            Self::Local(c) => write!(f, "(&container {:x})", c.raw_address()),
             Self::Global { status, container } => write!(
                 f,
-                "({:?}, {}, {})",
-                &status.borrow(),
-                container.rc_count(),
-                container
+                "(&container {:x} -- {:?})",
+                container.raw_address(),
+                &*status.borrow(),
             ),
         }
     }
@@ -2643,12 +2668,14 @@ impl Display for ContainerRef {
 
 impl Display for IndexedRef {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}<{}>", self.container_ref, self.idx)
+        write!(f, "{}[{}]", self.container_ref, self.idx)
     }
 }
 
 impl Display for Container {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(container {:x}: ", self.raw_address())?;
+
         match self {
             Self::Locals(r) | Self::Vec(r) | Self::Struct(r) => {
                 display_list_of_items(r.borrow().iter(), f)
@@ -2661,7 +2688,9 @@ impl Display for Container {
             Self::VecU256(r) => display_list_of_items(r.borrow().iter(), f),
             Self::VecBool(r) => display_list_of_items(r.borrow().iter(), f),
             Self::VecAddress(r) => display_list_of_items(r.borrow().iter(), f),
-        }
+        }?;
+
+        write!(f, ")")
     }
 }
 
@@ -3188,6 +3217,19 @@ impl Value {
     pub fn deserialize_constant(constant: &Constant) -> Option<Value> {
         let layout = Self::constant_sig_token_to_layout(&constant.type_)?;
         Value::simple_deserialize(&constant.data, &layout)
+    }
+}
+
+/***************************************************************************************
+*
+* Destructors
+*
+**************************************************************************************/
+// Locals may contain reference values that points to the same cotnainer through Rc, hencing forming
+// a cycle. Therefore values need to be manually taken out of the Locals in order to not leak memory.
+impl Drop for Locals {
+    fn drop(&mut self) {
+        _ = self.drop_all_values();
     }
 }
 

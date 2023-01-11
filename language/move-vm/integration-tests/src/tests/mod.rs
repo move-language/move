9ba4d3d40d4c59b0afdf905ace949b0d795a51e8
Teng Zhang <rahxephon89@163.com>
2023-01-10 16:17:08 -0800

    Cherry-pick the fix of limit instantiation recursion for all type instantiation onto aptos-main (#814)
    * Limit instantiation recursion for all type instantiation
    
    * fix minor
    
    Co-authored-by: Dario Russi <dario@aptoslabs.com>

--- language/move-vm/integration-tests/src/tests/mod.rs
+++ language/move-vm/integration-tests/src/tests/mod.rs
@@ -7,6 +7,7 @@ mod bad_storage_tests;
 mod binary_format_version;
 mod exec_func_effects_tests;
 mod function_arg_tests;
+mod instantiation_tests;
 mod invariant_violation_tests;
 mod leak_tests;
 mod loader_tests;

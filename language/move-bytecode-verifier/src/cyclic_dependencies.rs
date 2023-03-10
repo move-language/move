4d78d5b94949885c18d08198dc084069c1ea8a82
Ashok Menon <ashok@mystenlabs.com>
2023-03-10 22:09:56 +0000

    [loader] Do not traverse friends when verifying before load
    For Sui, packages are immutable and it is not practically possible to
    introduce a friendship across packages: the dependent package would
    need to be published first, and would need to know the address
    containing the friend at that time, but this is not possible because a
    package's address is decided on publish.
    
    We can take advantage of these properties to simplify loading by
    assuming that a `friend ...::B` declaration in module `A` is only
    relevant if `B` depends on `A`, and will be factored into verification
    when verifying/loading `B` (in which case the visibility of `A`'s
    functions to `B` is relevant).
    
    Furthermore, it should not matter if `A`'s friend module does not
    exist, and no part of `A`'s friends are referenced when `A` is loaded,
    so they do not need to be known to the loader.
    
    Test Plan:
    
    ```
    cargo nextest
    ```

--- language/move-bytecode-verifier/src/cyclic_dependencies.rs
+++ language/move-bytecode-verifier/src/cyclic_dependencies.rs
@@ -11,105 +11,53 @@ use move_binary_format::{
 use move_core_types::{language_storage::ModuleId, vm_status::StatusCode};
 use std::collections::BTreeSet;
 
-// This function performs a DFS in the module graph starting from each node in `items_to_explore`
-// and explores the neighbors of a node using the `immediate_nexts` closure.
-//
-// During the DFS,
-// - 1) if the `target_module_id` is found, the exploration will be short-circuited and returns a
-//   PartialVMError bearing the StatusCode specified in `error_on_cycle`.
-// - 2) if the `target_module_id` is not found, the modules visited in the DFS will be returned at
-//   the end of function execution.
-fn collect_all_with_cycle_detection<F: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>>(
-    target_module_id: &ModuleId,
-    items_to_explore: &[ModuleId],
-    immediate_nexts: &F,
-    error_on_cycle: StatusCode,
-) -> PartialVMResult<BTreeSet<ModuleId>> {
-    fn collect_all_with_cycle_detection_recursive<
-        F: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
-    >(
-        target_module_id: &ModuleId,
-        cursor_module_id: &ModuleId,
-        immediate_nexts: &F,
-        visited_modules: &mut BTreeSet<ModuleId>,
-    ) -> PartialVMResult<bool> {
-        if cursor_module_id == target_module_id {
+pub fn verify_module<D>(module: &CompiledModule, imm_deps: D) -> VMResult<()>
+where
+    D: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
+{
+    verify_module_impl(module, imm_deps).map_err(|e| e.finish(Location::Module(module.self_id())))
+}
+
+/// This function performs a depth-first traversal in the module graph, starting at `module` and
+/// recursively exploring immediate dependencies.  During the DFS,
+/// - If `module.self_id()` is encountered (again), a dependency cycle is detected and an error is
+///   returned.
+/// - Otherwise terminates without an error.
+fn verify_module_impl<D>(module: &CompiledModule, imm_deps: D) -> PartialVMResult<()>
+where
+    D: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
+{
+    fn detect_cycles<D>(
+        target: &ModuleId,
+        cursor: &ModuleId,
+        visited: &mut BTreeSet<ModuleId>,
+        deps: &D,
+    ) -> PartialVMResult<bool>
+    where
+        D: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
+    {
+        if cursor == target {
             return Ok(true);
         }
-        if visited_modules.insert(cursor_module_id.clone()) {
-            for next in immediate_nexts(cursor_module_id)? {
-                if collect_all_with_cycle_detection_recursive(
-                    target_module_id,
-                    &next,
-                    immediate_nexts,
-                    visited_modules,
-                )? {
+
+        if !visited.insert(cursor.clone()) {
+            for dep in deps(cursor)? {
+                if detect_cycles(target, &dep, visited, deps)? {
                     return Ok(true);
                 }
             }
         }
+
         Ok(false)
     }
 
-    let mut visited_modules = BTreeSet::new();
-    for item in items_to_explore {
-        if collect_all_with_cycle_detection_recursive(
-            target_module_id,
-            item,
-            immediate_nexts,
-            &mut visited_modules,
-        )? {
-            return Err(PartialVMError::new(error_on_cycle));
+    let self_id = module.self_id();
+    let mut visited = BTreeSet::new();
+    for dep in module.immediate_dependencies() {
+        if detect_cycles(&self_id, &dep, &mut visited, &imm_deps)? {
+            return Err(PartialVMError::new(StatusCode::CYCLIC_MODULE_DEPENDENCY));
         }
     }
-    Ok(visited_modules)
-}
 
-pub fn verify_module<D, F>(module: &CompiledModule, imm_deps: D, imm_friends: F) -> VMResult<()>
-where
-    D: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
-    F: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
-{
-    verify_module_impl(module, imm_deps, imm_friends)
-        .map_err(|e| e.finish(Location::Module(module.self_id())))
-}
-
-fn verify_module_impl<D, F>(
-    module: &CompiledModule,
-    imm_deps: D,
-    imm_friends: F,
-) -> PartialVMResult<()>
-where
-    D: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
-    F: Fn(&ModuleId) -> PartialVMResult<Vec<ModuleId>>,
-{
-    let self_id = module.self_id();
-
-    // collect and check that there is no cyclic dependency relation
-    let all_deps = collect_all_with_cycle_detection(
-        &self_id,
-        &module.immediate_dependencies(),
-        &imm_deps,
-        StatusCode::CYCLIC_MODULE_DEPENDENCY,
-    )?;
-
-    // collect and check that there is no cyclic friend relation
-    let all_friends = collect_all_with_cycle_detection(
-        &self_id,
-        &module.immediate_friends(),
-        &imm_friends,
-        StatusCode::CYCLIC_MODULE_FRIENDSHIP,
-    )?;
-
-    // check that any direct/transitive dependency is neither a direct nor transitive friend
-    match all_deps.intersection(&all_friends).next() {
-        Some(overlap) => Err(PartialVMError::new(
-            StatusCode::INVALID_FRIEND_DECL_WITH_MODULES_IN_DEPENDENCIES,
-        )
-        .with_message(format!(
-            "At least one module, {}, appears in both the dependency set and the friend set",
-            overlap
-        ))),
-        None => Ok(()),
-    }
+    Ok(())
 }

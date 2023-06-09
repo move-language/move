e7448cddf8bca8a8cef58bc291f0689ef65a3488
Timothy Zakian <tim@mystenlabs.com>
2023-06-09 09:40:43 -0700

    Bound runtime value depth for Move values

--- language/move-vm/runtime/src/config.rs
+++ language/move-vm/runtime/src/config.rs
@@ -4,6 +4,8 @@
 use move_binary_format::file_format_common::VERSION_MAX;
 use move_bytecode_verifier::VerifierConfig;
 
+pub const DEFAULT_MAX_VALUE_NEST_DEPTH: u64 = 128;
+
 /// Dynamic config options for the Move VM.
 pub struct VMConfig {
     pub verifier: VerifierConfig,
@@ -11,6 +13,8 @@ pub struct VMConfig {
     // When this flag is set to true, MoveVM will perform type check at every instruction
     // execution to ensure that type safety cannot be violated at runtime.
     pub paranoid_type_checks: bool,
+    /// Maximum value nest depth for structs
+    pub max_value_nest_depth: Option<u64>,
 }
 
 impl Default for VMConfig {
@@ -19,6 +23,7 @@ impl Default for VMConfig {
             verifier: VerifierConfig::default(),
             max_binary_format_version: VERSION_MAX,
             paranoid_type_checks: false,
+            max_value_nest_depth: Some(DEFAULT_MAX_VALUE_NEST_DEPTH),
         }
     }
 }

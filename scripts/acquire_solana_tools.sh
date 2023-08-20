2f79f412e60ca33b2bfa08890826e24c1e21313b
yihau <yihau.chen@icloud.com>
2023-08-21 03:57:47 +0800

    ci: add strategy for unit-test

---
+++ scripts/acquire_solana_tools.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+export MOVE_DEV_PATH
+export PLATFORM_TOOLS_PATH
+
+# platform tools version
+version=v1.37
+
+# check os and arch
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+infix=
+case $OS in
+Linux)
+  infix=linux-x86_64
+  ;;
+Darwin)
+  case $ARCH in
+  x86_64)
+    infix=osx-x86_64
+    ;;
+  *)
+    echo "macOS on unexpected arch: $ARCH"
+    exit 1
+    ;;
+  esac
+  ;;
+*)
+  echo "unexpected OS: $OS"
+  exit 1
+  ;;
+esac
+
+# install move dev
+root_path=solana-tools/llvm
+filename="move-dev-$infix.tar.bz2"
+
+mkdir -p "$root_path"
+curl -L "https://github.com/solana-labs/platform-tools/releases/download/$version/$filename" \
+  -o "$root_path/$filename"
+
+pushd "$root_path" || exit 1
+tar xjf "$filename"
+
+MOVE_DEV_PATH="$(pwd)/move-dev"
+popd || exit 1
+
+# install platform tools
+root_path=solana-tools/platform-tools
+filename="platform-tools-$infix.tar.bz2"
+
+mkdir -p "$root_path"
+curl -L "https://github.com/solana-labs/platform-tools/releases/download/$version/$filename" \
+  -o "$root_path/$filename"
+
+pushd "$root_path" || exit 1
+tar xjf "$filename"
+
+PLATFORM_TOOLS_PATH=$(pwd)
+popd || exit 1

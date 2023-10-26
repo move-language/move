66575d4653cd093d39dcfae2df48388453cd74d9
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-10-26 17:00:55 +0200

    feat: add common types for move-vm-backend

---
+++ move-vm-backend-common/src/lib.rs
@@ -0,0 +1,5 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+pub mod types;
---
+++ move-vm-backend-common/src/types.rs
@@ -0,0 +1,37 @@
+use alloc::vec::Vec;
+use anyhow::{Error, Result};
+use serde::{Deserialize, Serialize};
+
+/// Package contains a list of module bytecodes.
+#[derive(Serialize, Deserialize)]
+pub struct ModulePackage {
+    /// Module bytecodes.
+    modules: Vec<Vec<u8>>,
+}
+
+impl ModulePackage {
+    /// Create a new ModulePackage.
+    pub fn new(modules: Vec<Vec<u8>>) -> Self {
+        Self {
+            modules
+        }
+    }
+
+    /// Gets module bytecodes.
+    pub fn into_inner(self) -> Vec<Vec<u8>> {
+        self.modules
+    }
+
+    /// Serializes data.
+    pub fn encode(self) -> Result<Vec<u8>> {
+        bcs::to_bytes(&self).map_err(Error::msg)
+    }
+}
+
+impl TryFrom<&[u8]> for ModulePackage {
+    type Error = Error;
+
+    fn try_from(blob: &[u8]) -> Result<Self, Self::Error> {
+        bcs::from_bytes(blob).map_err(Error::msg)
+    }
+}

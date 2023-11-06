852dc03d7e16965c0ea448966126ac936c205108
35359595 <35359595i@gmail.com>
2023-11-06 14:19:59 -0800

    tests extended with new parameter

--- move-vm-backend/tests/move_vm.rs
+++ move-vm-backend/tests/move_vm.rs
@@ -1,9 +1,8 @@
 use crate::mock::StorageMock;
-use move_vm_backend::Mvm;
-
 use move_core_types::account_address::AccountAddress;
 use move_core_types::identifier::Identifier;
 use move_core_types::language_storage::StructTag;
+use move_vm_backend::{Mvm, TransferError};
 use move_vm_backend_common::types::ModuleBundle;
 
 use move_core_types::language_storage::TypeTag;
@@ -58,12 +57,20 @@ fn read_script_bytes_from_project(project: &str, script_name: &str) -> Vec<u8> {
     read_bytes(&path)
 }
 
+fn fake_transfer(
+    _from: AccountAddress,
+    _to: AccountAddress,
+    _amount: u128,
+) -> Result<(), TransferError> {
+    Ok(())
+}
+
 #[test]
 #[ignore = "we need to build the move package before with a script before running the test"]
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn publish_module_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let address = AccountAddress::from_hex_literal("0xCAFE").unwrap();
     let module = read_module_bytes_from_project("empty", "Empty");
@@ -80,7 +87,7 @@ fn publish_module_test() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn publish_module_package_from_multiple_module_files() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let module_1 = read_module_bytes_from_project("using_stdlib_natives", "Vector");
@@ -96,7 +103,7 @@ fn publish_module_package_from_multiple_module_files() {
 
     // Recreate the storage and the MoveVM
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     // Order matters - we cannot publish module_2 before module_1!
     let modules = ModuleBundle::new(vec![module_2, module_1])
         .encode()
@@ -113,7 +120,7 @@ fn publish_module_package_from_multiple_module_files() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn publish_module_package_from_bundle_file() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let bundle = read_bundle_from_project("using_stdlib_natives", "using_stdlib_natives");
@@ -129,7 +136,7 @@ fn publish_module_package_from_bundle_file() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn publish_module_dependent_on_stdlib_natives() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let mod_using_stdlib_natives = read_module_bytes_from_project("using_stdlib_natives", "Vector");
@@ -161,7 +168,7 @@ fn publish_module_dependent_on_stdlib_natives() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn publish_module_using_stdlib_full_fails() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let mod_using_stdlib_natives =
@@ -183,7 +190,7 @@ fn publish_module_using_stdlib_full_fails() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn get_module_and_module_abi() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let module = read_module_bytes_from_project("using_stdlib_natives", "Vector");
     let address = AccountAddress::from_hex_literal("0x2").unwrap();
@@ -208,7 +215,7 @@ fn get_module_and_module_abi() {
 // This test heavily depends on Move.toml files for thes used Move packages.
 fn get_resource() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let addr_std = AccountAddress::from_hex_literal("0x1").unwrap();
@@ -265,7 +272,7 @@ fn get_resource() {
 #[ignore = "we need to build the move package before with a script before running the test"]
 fn execute_script_with_no_params_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let script = read_script_bytes_from_project("simple_scripts", "empty_loop");
 
@@ -283,7 +290,7 @@ fn execute_script_with_no_params_test() {
 #[ignore = "we need to build the move package before with a script before running the test"]
 fn execute_script_params_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let script = read_script_bytes_from_project("simple_scripts", "empty_loop_param");
 
@@ -302,7 +309,7 @@ fn execute_script_params_test() {
 #[ignore = "we need to build the move package before with a script before running the test"]
 fn execute_script_generics_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let script = read_script_bytes_from_project("simple_scripts", "generic_1");
 
@@ -330,7 +337,7 @@ fn execute_script_generics_test() {
 #[ignore = "we need to build the move package before with a script before running the test"]
 fn execute_script_generics_incorrect_params_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
 
     let script = read_script_bytes_from_project("simple_scripts", "generic_1");
 
@@ -359,7 +366,7 @@ fn execute_script_generics_incorrect_params_test() {
 #[ignore = "we need to build the move package before with a script before running the test"]
 fn execute_function_test() {
     let store = StorageMock::new();
-    let vm = Mvm::new(store).unwrap();
+    let vm = Mvm::new(store, Box::new(fake_transfer)).unwrap();
     let mut gas_status = GasStatus::new_unmetered();
 
     let addr_std = AccountAddress::from_hex_literal("0x1").unwrap();
